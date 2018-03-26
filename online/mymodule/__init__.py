ed7e72576757cbc68acc08f6248c0cf4eab5a1b2
justwangweimin <26206594/@qq.com>
2018-03-26 22:31:56 +0800

    first commit

--- online/mymodule/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @File  : run_model.py.py
-# @Author: zjj421
-# @Date  : 17-8-27
-# @Desc  :
-
-
--- online/mymodule/extensions/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @File  : __init__.py.py
-# @Author: zjj421
-# @Date  : 17-9-11
-# @Desc  :
